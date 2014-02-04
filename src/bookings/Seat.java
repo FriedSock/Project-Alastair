4e88e63564111d15e3145b987a5b468aca1c5d83
Craig Ellis <cellis_222@hotmail.com>
2014-02-04 17:36:25 +0000

    NoWarn IndexNegative

--- src/bookings/Seat.java
+++ src/bookings/Seat.java
@@ -7,18 +7,24 @@ public class Seat {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 20;
 
+    //@ invariant row >= MIN_ROW && row <= MAX_ROW
     private final char row;
+    //@ invariant number >= MIN_NUMBER && number <= MAX_NUMBER
     private final int number;
 
+    //@ requires row >= MIN_ROW && row <= MAX_ROW
+    //@ requires number >= MIN_NUMBER && number <= MAX_NUMBER
     public Seat(char row, int number) {
         this.row = row;
         this.number = number;
     }
 
+    //@ ensures \result >= MIN_ROW && \result <= MAX_ROW
     public final char getRow() {
         return row;
     }
 
+    //@ ensures \result >= MIN_NUMBER && \result <= MAX_NUMBER
     public final int getNumber() {
         return number;
     }
