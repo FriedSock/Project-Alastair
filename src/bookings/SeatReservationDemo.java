87cfef72e3e479c709206ba8969ad25f87b20c44
Milan Misak <misakmilan@gmail.com>
2014-02-05 14:36:25 +0000

    Daikon

--- src/bookings/SeatReservationDemo.java
+++ src/bookings/SeatReservationDemo.java
@@ -6,10 +6,16 @@ public class SeatReservationDemo {
         SeatReservationManager manager = new SeatReservationManager();
 
         manager.reserve(new Seat('A', 1), new Customer());
+        manager.reserve(new Seat('A', 2), new Customer());
         manager.reserve(new Seat('D', 5), new Customer());
         manager.reserve(new Seat('F', 12), new Customer());
         manager.reserve(new Seat('G', 19), new Customer());
 
+        // Try booking a booked seat
+        try {
+            manager.reserve(new Seat('A', 1), new Customer());
+        } catch (ReservationException e) {
+        }
     }
 
 }
