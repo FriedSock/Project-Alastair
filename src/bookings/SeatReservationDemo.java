b8692865534790db35e90490367611acdc9c93d2
Milan Misak <misakmilan@gmail.com>
2014-02-05 14:02:08 +0000

    First version of the main method

--- src/bookings/SeatReservationDemo.java
+++ src/bookings/SeatReservationDemo.java
@@ -3,6 +3,12 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
+        SeatReservationManager manager = new SeatReservationManager();
+
+        manager.reserve(new Seat('A', 1), new Customer());
+        manager.reserve(new Seat('D', 5), new Customer());
+        manager.reserve(new Seat('F', 12), new Customer());
+        manager.reserve(new Seat('G', 19), new Customer());
 
     }
 
