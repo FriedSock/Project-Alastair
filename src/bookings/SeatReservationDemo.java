2a2360c97f840584493cd36e2549b87f1e3d721b
Craig Ellis <cellis_222@hotmail.com>
2014-02-05 15:46:40 +0000

    removed some invariants, wrote it up

--- src/bookings/SeatReservationDemo.java
+++ src/bookings/SeatReservationDemo.java
@@ -7,14 +7,56 @@ public class SeatReservationDemo {
 
         manager.reserve(new Seat('A', 1), new Customer());
         manager.reserve(new Seat('A', 2), new Customer());
-        manager.reserve(new Seat('D', 5), new Customer());
+        manager.reserve(new Seat('D', 7), new Customer());
         manager.reserve(new Seat('F', 12), new Customer());
-        manager.reserve(new Seat('G', 19), new Customer());
+        manager.reserve(new Seat('G', 20), new Customer());
 
         // Try booking a booked seat
         try {
             manager.reserve(new Seat('A', 1), new Customer());
         } catch (ReservationException e) {
+        }
+
+        // Try booking a booked seat
+        try {
+            manager.reserve(new Seat('D', 7), new Customer());
+        } catch (ReservationException e) {
+        }
+
+        // Try booking a booked seat
+        try {
+            manager.reserve(new Seat('F', 12), new Customer());
+        } catch (ReservationException e) {
+        }
+
+        // Try booking a booked seat
+        try {
+            manager.reserve(new Seat('G', 20), new Customer());
+        } catch (ReservationException e) {
+        }
+
+        // Try booking a non-existent seat
+        try {
+            manager.reserve(new Seat('A', -100), new Customer());
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+
+        // Try booking a non-existent seat
+        try {
+            manager.reserve(new Seat('A', 100), new Customer());
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+
+        // Try booking a non-existent seat
+        try {
+            manager.reserve(new Seat((char) 0, 1), new Customer());
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+
+        // Try booking a non-existent seat
+        try {
+            manager.reserve(new Seat('Z', 1), new Customer());
+        } catch (ArrayIndexOutOfBoundsException e) {
         }
     }
 
