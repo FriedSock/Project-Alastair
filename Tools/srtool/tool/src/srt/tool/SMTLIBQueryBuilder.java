dab9885687917723f6f71acdcf1cd4ad65a2b5e5
Jack Bracewell <jbtwentythree@gmail.com>
2014-03-04 17:33:31 +0000

    Part 3 working for the simple test case
    Only addition binary operator has been added, need to do the others

--- Tools/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
+++ Tools/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
@@ -1,5 +1,9 @@
 package srt.tool;
 
+import srt.ast.AssertStmt;
+import srt.ast.AssignStmt;
+import srt.ast.Expr;
+
 public class SMTLIBQueryBuilder {
 
 	private ExprToSmtlibVisitor exprConverter;
@@ -13,12 +17,26 @@ public class SMTLIBQueryBuilder {
 
 	public void buildQuery() {
 		StringBuilder query = new StringBuilder();
+		ExprToSmtlibVisitor exprToSmt = new ExprToSmtlibVisitor();
+		
 		query.append("(set-logic QF_BV)\n"
 				+ "(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
-		// TODO: Define more functions above (for convenience), as needed.
 
-		// TODO: Declare variables, add constraints, add properties to check
-		// here.
+		for (String variableName : constraints.variableNames) {
+			query.append("(declare-fun " + variableName + " () (_ BitVec 32))\n");
+		}
+		
+		for (AssignStmt assStmt : constraints.transitionNodes) {
+			String name = assStmt.getLhs().getName();
+			String rhs = exprToSmt.visit(assStmt.getRhs());
+			query.append("(assert (= " + name + " " + rhs + "))\n");
+		}
+		
+		for (AssertStmt asrtStmt : constraints.propertyNodes) {
+			String expr = exprToSmt.visit(asrtStmt.getCondition());
+			query.append("(assert (not " + expr + "))\n");
+		}
+
 
 		query.append("(check-sat)\n");
 		queryString = query.toString();
